426fc1ba93cf8a75646c8ac830cd4968d1791b1a
Kekoa Kubli <kkubli3246@gmail.com>
2019-01-22 00:04:36 -0700

    first commit blocchat room list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD4bpzDfvsj6KiP7E7bhzDh_Fq54zRTTTs",
+    authDomain: "blocchat-rooms.firebaseapp.com",
+    databaseURL: "https://blocchat-rooms.firebaseio.com",
+    projectId: "blocchat-rooms",
+    storageBucket: "blocchat-rooms.appspot.com",
+    messagingSenderId: "389662709234"
+  };
+  firebase.initializeApp(config);
+
+
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      rooms: []
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <RoomList rooms ="firebase"/>
       </div>
     );
   }
