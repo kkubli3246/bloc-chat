b44d912c486c6d64d21ca783768e04223ee56e3b
Kekoa Kubli <kkubli3246@gmail.com>
2019-01-23 22:36:30 -0700

    bloc chat create new chat room method complete

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -6,7 +6,8 @@ class RoomList extends Component{
 constructor(props){
     super(props);
     this.state ={
-        rooms: []        
+        rooms: [],   
+        newRoomName:''     
     };
     
      this.roomsRef = this.props.firebase.database().ref('rooms');
@@ -19,15 +20,39 @@ componentDidMount(){
        this.setState({ rooms: this.state.rooms.concat( room ) })
     });
 }
+
+handleSubmit = (e) =>{
+    e.preventDefault();
+    this.createRoom();
+    this.setState({newRoomName: ''})
+}
+createRoom = () =>{
+    if(this.state.newRoomName.replace(/\s/g, '').length > 0 )
+    {this.roomsRef.push({
+        name: this.state.newRoomName
+    });}
+}
+handleChange =(e) =>{
+    this.setState({newRoomName: e.target.value})
+}
     render(){
       return(
           <div className="room-list">
-             
+            <div id="room-lists">             
              {this.state.rooms.map((room,i) =>
                 <h2 key={i}>
                     {room.name}
                 </h2>
              )}
+             <form onSubmit ={(e) => this.handleSubmit(e)}>
+                 <input 
+                    type="text" 
+                    value={this.state.newRoomName}
+                    onChange ={this.handleChange}
+                 ></input>
+                 <input type="submit"></input>
+             </form>
+             </div>
           </div>
       );
     }
